5d11fd531e43179aa6c36d8af1b907de8865f770
Charlton Roberts <charltonroberts@gmail.com>
2015-09-27 18:01:41 -0400

    Fixes typo with env variable checking

--- kinesis/index.js
+++ kinesis/index.js
@@ -3,7 +3,7 @@ let kinesiserapy = require('kinesiserapy');
 class Kinesis {
 
 	constructor(){
-		if ( "AWS_KINESES_STREAM" in process.env    &&
+		if ( "AWS_KINESIS_STREAM" in process.env    &&
 				 "AWS_ACCESS_KEY_ID" in process.env     &&
 				 "AWS_SECRET_ACCESS_KEY" in process.env &&
 				 "AWS_REGION" in process.env ) {
@@ -15,10 +15,14 @@ class Kinesis {
 				AWS_REGION            : process.env.AWS_REGION
 			};
 
-			this.emitter = new kinesis.KEmitter(this.stream, this.auth);
+			this.emitter = new kinesiserapy.KEmitter(this.stream, this.auth);
 		}
 		else {
-			console.error('\n\nAWS config not set in environment variables\n\n');
+			console.error('\n\nAWS config not set in environment variables:');
+			console.error('AWS_KINESIS_STREAM: ' + process.env.AWS_KINESIS_STREAM);
+			console.error('AWS_REGION: ' + process.env.AWS_REGION);
+			console.error('AWS_ACCESS_KEY_ID: ' + (typeof process.env.AWS_ACCESS_KEY_ID === "string")? "set": "UNSET");
+			console.error('AWS_SECRET_ACCESS_KEY: ' + (typeof process.env.AWS_SECRET_ACCESS_KEY === "string")? "set": "UNSET");
 			process.exit(1);
 		}
 	}
